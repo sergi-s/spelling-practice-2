46c93588e6e372b6b5fe6914440f498e3c2725c2
Sergi Rizkallah <sergi@sergis-air.home>
2024-04-26 18:40:02 -0230

    design database

---
+++ src/app/api/phrase/route.ts
@@ -0,0 +1,18 @@
+// import natural, { type DataRecordsCallback, type Stemmer } from 'natural';
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import { NextResponse } from 'next/server';
+// import { z } from 'zod';
+
+
+// TODO: get  words from the database
+// const wordnet = new natural.WordNet("./node_modules/wordnet-db/dict");
+// const tokenizer = new natural.WordTokenizer();
+
+// export const GET = async (
+//     req: NextApiRequest,
+// ) => {
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+//     const userId = 1
+
+// }
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,64 @@
 "use client"
 import { useState } from "react";
-import Speech from 'react-speech' as any;
+// import Speech from 'react-speech';
+import { useSpeechSynthesis } from 'react-speech-kit';
+ 
 
 
-async function fetchData() {
-  // Define the URL endpoint you want to send the request to
-  const url = 'http://localhost:3000/api/stem';
 
-  // Define the options for the fetch request (e.g., method, headers, body)
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    // Add body for POST or PUT requests
-    body: JSON.stringify({ language: 'english', phrase: "hello world" }),
-  };
+// async function fetchData() {
+//   // Define the URL endpoint you want to send the request to
+//   const url = 'http://localhost:3000/api/stem';
 
-  try {
-    // Make the fetch request
-    const response = await fetch(url, options);
+//   // Define the options for the fetch request (e.g., method, headers, body)
+//   const options = {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     // Add body for POST or PUT requests
+//     body: JSON.stringify({ language: 'english', phrase: "hello world" }),
+//   };
 
-    // Check if the request was successful
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
+//   try {
+//     // Make the fetch request
+//     const response = await fetch(url, options);
 
-    // Parse the response as JSON
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const data = await response.json();
+//     // Check if the request was successful
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-    return data
-  } catch (error) {
-    // Handle any errors that occurred during the fetch request
-    console.error('There was a problem with the fetch operation:', error);
-  }
-}
+//     // Parse the response as JSON
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const data = await response.json();
+
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+//     return data
+//   } catch (error) {
+//     // Handle any errors that occurred during the fetch request
+//     console.error('There was a problem with the fetch operation:', error);
+//   }
+// }
 
 
 export default function HomePage() {
 
   const [text, setText] = useState('how much wood would a woodchuck chuck if the woodchuck could chuck wood');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const { speak } = useSpeechSynthesis()
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-        <Speech
+        {/* <Speech
           textAsButton={true}
           displayText="Hello"
           lang="en-GB"
           voice="Google UK English Male"
-          text={text} />
+          text={text} /> */}
+          {/* eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call */}
+          <button onClick={() => speak({ text })}>Speak</button>
       </div>
     </main>
   );
