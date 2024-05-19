2f1f2bc62fb41cfd3377c285fcef671ced4b9fc2
coco-kit <bofg.fu@outlook.com>
2024-05-19 09:54:59 -0230

    animation

--- src/app/spelling/SpellingComparison.tsx
+++ src/app/spelling/SpellingComparison.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
+import React from "react";
 
-export const SpellingComparison = ({ correctPhrase, missSpelledWords }: { correctPhrase: string[], missSpelledWords: string[] }) => {
-    return (
-        <div className='col-span-2 px-4 py-2 bg-white text-black rounded border-2 border-gray-300 focus:outline-none focus:border-blue-500'>
-            {correctPhrase.map((word, index) => (
-                <span
-                    key={index}
-                    style={{ color: missSpelledWords.includes(word) ? 'red' : 'green' }}
-                >
-                    {word}
-                    {' '}
-                </span>
-            ))}
-        </div>
-    );
+export const SpellingComparison = ({
+  correctPhrase,
+  missSpelledWords,
+}: {
+  correctPhrase: string[];
+  missSpelledWords: string[];
+}) => {
+  return (
+    // <div className='col-span-2 px-4 py-2 bg-white text-black rounded border-2 border-gray-300 focus:outline-none focus:border-blue-500'>
+    //     {correctPhrase.map((word, index) => (
+    //         <span
+    //             key={index}
+    //             style={{ color: missSpelledWords.includes(word) ? 'red' : 'green' }}
+    //         >
+    //             {word}
+    //             {' '}
+    //         </span>
+    //     ))}
+    // </div>
+
+    <div className="card-wrapper h-[50px] w-[500px]">
+      <div className="card-content col-span-2 px-4 py-2">
+        {correctPhrase.map((word, index) => (
+          <span
+            key={index}
+            style={{ color: missSpelledWords.includes(word) ? "red" : "green" }}
+          >
+            {word}{" "}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ export default {
   ],
   theme: {
     extend: {
+      keyframes: {
+        'border-spin': {
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+      },
+      animation: {
+        'border-spin': 'border-spin 7s linear infinite',
+      },
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
