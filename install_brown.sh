8ce571d61bc709c147eb8264976f330e61a47cbd
sergi-s <sergisamirboules@gmail.com>
2024-06-08 19:15:41 -0230

    pwd to download corps in the right place

--- install_brown.sh
+++ install_brown.sh
@@ -6,18 +6,24 @@ ZIP_URL="https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/corp
 # Define the folder name
 FOLDER_NAME="brown"
 
+# Get the absolute path of the current directory
+CURRENT_DIR=$(pwd)
+
+# Define the absolute path for extraction
+EXTRACTION_PATH="$CURRENT_DIR/$FOLDER_NAME"
+
 # Download the zip file using curl
 curl -o "$FOLDER_NAME.zip" "$ZIP_URL"
 
 # Check if download was successful
 if [ $? -eq 0 ]; then
-    # Extract the contents of the zip file
-    unzip "$FOLDER_NAME.zip" -d .
+    # Extract the contents of the zip file to the specified directory
+    unzip "$FOLDER_NAME.zip" -d "$CURRENT_DIR"
     # Check if extraction was successful
     if [ $? -eq 0 ]; then
-        echo "Extraction successful. Files are located in the current directory."
+        echo "Extraction successful. Files are located in the directory: $EXTRACTION_PATH"
 
-        echo "corps" >>"$FOLDER_NAME"/.gitkeep
+        echo "corps" >> "$EXTRACTION_PATH"/.gitkeep
     else
         echo "Error: Extraction failed."
     fi
