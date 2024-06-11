a6e8fdcd7e33c77741228dffcfdcabc5d8d29d2a
sergi-s <sergisamirboules@gmail.com>
2024-06-11 20:09:51 -0230

    try to push corps installation and parsing again

--- src/app/api/globalVariables.ts
+++ src/app/api/globalVariables.ts
@@ -1,10 +1,10 @@
-// import { tokenizeCorpus } from '../utils/NLP/tokenizeCorpus';
-// import { createFrequencyDocument } from '../utils/NLP/createfrequencyDocument';
+import { tokenizeCorpus } from '../utils/NLP/tokenizeCorpus';
+import { createFrequencyDocument } from '../utils/NLP/createFrequencyDocument';
 import { db } from '~/server/db';
 
 
 export const prisma = db
 
 
-export const documents: string[] = []//tokenizeCorpus();
-export const wordFrequencyScores: Record<string, number> = {} //createFrequencyDocument(documents);
+export const documents: string[] = tokenizeCorpus();
+export const wordFrequencyScores: Record<string, number> = createFrequencyDocument(documents);
