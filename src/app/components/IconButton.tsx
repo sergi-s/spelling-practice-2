91408b38c0c99bcf4c99ad1aa829c07887e59f83
sergi-s <sergisamirboules@gmail.com>
2024-06-14 09:11:03 -0230

    fix build errors

--- src/app/components/IconButton.tsx
+++ src/app/components/IconButton.tsx
@@ -8,6 +8,7 @@ type IconButtonProps = {
   id?: string;
 };
 
+// eslint-disable-next-line react/display-name
 const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
   ({ onClick, color, children, id }, ref) => {
     const buttonClass = color === 'gradient1' ? styles.gradientButton1 : styles.gradientButton2;
